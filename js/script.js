4d2fad1f37cf3683a87a8957eb622ef3d919495e
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-10-11 02:06:25 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#slides').superslides({
+        animation:'fade',
+        play:5000,
+        pagination:false
+    });
+    var typed = new Typed(".typed",{
+        strings:["Web Developer.","Student.","Doctor."],
+        typeSpeed:70,
+        loop:true,
+        startDelay:1000,
+        showCursor:false
+    });
+});
